1c161e28d167cd9ea77d74e78451aa0a29675823
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-06 14:33:37 +0100

    solution for scope exercises

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('hello world!')
+/* SOLUTIONS FOR SCOPE EXERCISES */
+
+/* ---- Starting point ---- */
+
+/* var a;
+a = 5;
+console.log(a); */
+
+/* ---- Global variable is declared ---- */
+
+/* 
+//var a;
+a = 5;
+console.log(a); */
+
+/* ---- Reference error ---- */
+
+/*
+ //var a;
+ //a = 5;
+ console.log(a); */
+
+ /* ---- Reference error with strict mode ---- */
+
+/*
+'use strict';
+a = 5;
+console.log(a); */
+
+/* ---- Type error ---- */
+
+/* var f1;
+var a;
+ a = 5;
+console.log('A: ', f1());
+f1 = function () {
+  var b;
+  b = 6;
+  console.log('B: ', b);
+  return b;
+} */
