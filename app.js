a56f5eeeb2c627b1da0088e9773cba86cb058fb9
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-06 14:36:29 +0100

    nested scope exercises

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world!')
+/* SOLUTIONS FOR NESTED SCOPE EXERCISES */
+
+var f1;
+f1 = function () {
+  var b;
+  b = 6;
+  console.log('B: ', b);
+  return b + a;
+}
+var a;
+ a = 5;
+ console.log('A: ', f1());
