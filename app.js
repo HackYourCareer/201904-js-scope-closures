91f4348767efb99fd003d2fc5dd1edd66a07d36b
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-11 14:47:47 +0100

    dynamic scope exercises

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello world!')
+/* SOLUTIONS FOR DYNAMIC SCOPE EXERCISES */
+
+/* ---- Starting point ---- */
+
+function f2() {
+  var c;
+  c = 'very very long';
+  console.log('C: ', c + a);
+}
+var f1;
+f1 = function () {
+  var b;
+  b = 6;
+  var a = 10;
+  f2();
+  console.log('B: ', b);
+  return b + a;
+}
+var a;
+a = 5;
+console.log('A: ', f1());
+
