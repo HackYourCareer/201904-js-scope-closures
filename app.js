2a6b3653809c5d0cd06bbb94481a162766ff8a13
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-11 15:16:52 +0100

    closure exercises

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world!')
+/* SOLUTIONS FOR CLOSURE EXERCISES */
+
+/* ---- Starting point ---- */
+
+var f1;
+f1 = function () {
+  function f2() {
+    var c;
+    c = 'very very long';
+    console.log('C: ', c + a + b);
+  }
+  var b;
+  b = 6;
+  f2();
+  console.log('B: ', b);
+  return f2;
+}
+var a;
+a = 5;
+var f = f1();
+var b = 88;
+f();
+
