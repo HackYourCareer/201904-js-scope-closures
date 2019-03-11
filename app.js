1d71bb142c0ba6a3b394ede66c214c2239b557de
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-11 15:41:33 +0100

    IIFE exercises

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello world!')
+/* SOLUTIONS FOR IIFE EXERCISES */
+
+/* ---- Starting point ---- */
+
+var f1;
+f1 = function () {
+  var b;
+  b = 6;
+  (function () {
+    var c;
+    c = 'very very long';
+    console.log('C: ', c + a + b);
+  })();
+  console.log('B: ', b);
+}
+var a;
+a = 5;
+f1()
