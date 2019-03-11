c6bb8fee6199bf0c802ec0467a7232c7bef4fc07
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-11 14:28:51 +0100

    function scope exercises

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello world!')
+/* SOLUTIONS FOR FUNCTION SCOPE EXERCISES */
+
+/* ---- Starting point ---- */
+
+var f1;
+f1 = function () {
+  var b;
+  b = 6;
+  function f2() {
+    var c;
+    c = 'very very long';
+    console.log('C: ', c);
+  }
+  f2();
+  console.log('B: ', b + c);
+  return b + a;
+}
+var a;
+a = 5;
+console.log('A: ', f1());
