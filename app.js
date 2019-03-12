c013b3d0d404e788038b5e76454f7f63a9e0d735
Sporysz, Edyta <edyta.sporysz@sap.com>
2019-03-12 09:29:58 +0100

    module exercises

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello world!')
+/* SOLUTIONS FOR MODULE EXERCISES */
+
+/* ---- Starting point ---- */
+
+var m = (function() {
+  var a = 'short';
+  var c = 'very very long';
+  function f1() {
+    console.log('C: ', a + ' and ' + c);
+  }
+  return {
+    c: c,
+    f1: f1
+  }
+})();
+
+m.f1();
+/* console.log(m.c);
+console.log(m.a); */
