3d89d1539718bf9209ac014538ed4e67c6dc3c40
Krzysztof Krupa <krzysztof.krupa@sap.com>
2019-03-24 18:07:00 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello world!')
+var myLib = (function () {
+  function validate(form, validator) {
+    var isValid = validator(form);
+    if (!isValid) {
+      console.log('ERROR');
+    }
+  }
+
+  return {
+    validate: validate
+  }
+
+})();
+
+function max(form) {
+  if (form.field >= 3) {
+    return false;
+  }
+  return true;
+}
+
+var form = {
+  field: 5
+}
+
+myLib.validate(form, max);
