f36bc6f353a7fbdac9cc94af9db94b844c635460
Krzysztof Krupa <krzysztof.krupa@sap.com>
2019-04-08 21:13:48 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-/* SOLUTIONS FOR DYNAMIC SCOPE EXERCISES */
-
-/* ---- Starting point ---- */
-
 var a = 'A'; //LHS
 console.log('var a: ', a); //RHS
 function f2() {
   var c = 'C';
   console.log('var c + a: ', c + a);
 }
+
 function f1() {
   var b = 'B';
   var a = 'Y';
@@ -15,4 +12,3 @@ function f1() {
   console.log('var a + b: ', a + b);
 }
 f1();
-
