984d80d80c839b7abdefe1a3c43cfad2c42387c7
Krzysztof Krupa <krzysztof.krupa@sap.com>
2019-04-12 07:33:30 +0200

    solution

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello world!')
+var myLib = (function () {
+  function validate(from, validator) {
+    var isValid = validator(from);
+    if (isValid) {
+      console.log('VALID');
+    } else {
+      console.log('INVALID');
+    }
+  }
+  return {
+    validate: validate
+  }
+})();
+
+var form = {
+  field: 5
+}
+
+function max(limit) {
+  return function max(form) {
+    return form.field <= limit;
+  }
+}
+
+myLib.validate(form, max(10));
+myLib.validate(form, max(1));
